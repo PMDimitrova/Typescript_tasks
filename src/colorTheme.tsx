0abeef71434f3660741a449242d819ea1d5f8d5a
PMDimitrova <palmina.dimitrova@gmail.com>
2024-01-12 11:45:17 +0200

    Dev dependencies; Font & styling

---
+++ src/colorTheme.tsx
@@ -0,0 +1,9 @@
+const colorTheme = {
+    textLight: '#74a9b3',
+    textDark: '#067f94',
+    textDarker: '#5f5765',
+    attention: '#f255b3',
+    attentionSecondary: '#ffe070',
+    background: '#FEF5FF'
+} 
+export default colorTheme;
